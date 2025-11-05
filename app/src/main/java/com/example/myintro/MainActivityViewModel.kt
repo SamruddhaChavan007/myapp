f432d5d9cff93a9b1bb8721a2f51cf5f0a210948
SamruddhaChavan007 <75562378+SamruddhaChavan007@users.noreply.github.com>
2025-11-04 22:42:35 -0700

    Implemented SplashScreen

---
+++ app/src/main/java/com/example/myintro/MainActivityViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.myintro
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel: ViewModel() {
+
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(3000)
+            _isReady.value = true
+        }
+    }
+}
